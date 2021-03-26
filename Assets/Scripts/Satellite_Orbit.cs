fbba47b6628a7f49884e86b4663a75603072a262
Adam Bissett <abissett96@gmail.com>
2021-03-26 00:23:52 -0700

    Cleaned up Satelite_Orbit.cs/added orbital orientation
    Added a bunch of comments to Satelite_Orbit.cs and deleted unused variables. Added public variables for innclination, right ascension, and argument of perigee. Uses quaternions and rotation matrices to calculate ECI_mat. Only performs calculations on startup right now.

--- Assets/Scripts/Satellite_Orbit.cs
+++ Assets/Scripts/Satellite_Orbit.cs
@@ -4,32 +4,58 @@ using UnityEngine;
 
 public class Satellite_Orbit : MonoBehaviour
 {
-    float earth_rad;
-    static float mu = 6.673f * 5.972f * Mathf.Pow(10, 24 - 11 - 5 * 3); // G * M_E * (10^(-5))^3
-    float ra;
-    float rp;
-    float a;
-    float b;
-    float e;
-    float n;
-    float E;
-    float M;
-    int repititions = 100;
-    Vector3 pos;
+    /*** Variables ***/
 
+    // Physical Constants
+    static float earth_rad = 63.71f; // R_E * 10^(-5)
+    static float mu = 6.673f * 5.972f * Mathf.Pow(10, 24 - 11 - 5 * 3); // G * M_E * (10^(-5))^3
+
+    // Orbital Parameters
+    float ra;   // Apogee radius
+    float rp;   // Perigee radius
+    float a;    // Semimajor axis
+    float b;    // Semiminor axis
+    float e;    // Eccentricity
+    float n;    // Mean motion
+    float E;    // True anomilly 
+    float M;    // Mean anomilly
+    Quaternion q_w,     // w quaternion
+        q_inc,          // inc quaternion
+        q_omega;        // omega quaternion
+    Matrix4x4 ECI_mat;  // ECI orientation matrix
+
+    // Calulation Variables
+    int repititions = 100;  // Number of iterations to aproximate E
+    Vector3 pos;            // Position of the satelite
+
+    // Public Orbital Parameters
     public float apogee;
     public float perigee;
-    public float period;
-    public float time;
+    //public float period;
+    public float inc;       // Inclination angle
+    public float omega;     // Right ascension of ascending node
+    public float w;         // Argument of perigree
 
+    // Time Variables
     public GameObject clock;
+
+    // Game_Time script
     Game_Time game_time;
+
+    /*** Events ***/
 
     // Start is called before the first frame update
     void Start()
     {
-        // Defining the values of the constants
-        earth_rad = 63.71f;
+        // Calculate quaternions for (inc, omega, w)
+        q_w = Quaternion.Euler(0f, 0f, w);          // rotate w radians about y-axis
+        q_inc = Quaternion.Euler(0f, inc, 0f);      // rotate inc radians about x-axis
+        q_omega = Quaternion.Euler(0f, 0f, omega);  // rotate omega radians about y-axis
+
+        // Calculate the orientation matrix for the orbital plane
+        ECI_mat = Matrix4x4.Rotate(q_omega) * Matrix4x4.Rotate(q_inc) * Matrix4x4.Rotate(q_w);
+
+        // Defining the values of the orbital parameters
         ra = earth_rad + apogee / 100000.0f;
         rp = earth_rad + perigee / 100000.0f;
         a = (ra + rp) / 2;
@@ -41,13 +67,16 @@ public class Satellite_Orbit : MonoBehaviour
         // game_time.total_seconds is the total game-world seconds since the start of the program
         clock = GameObject.Find("Clock");
         game_time = clock.GetComponent<Game_Time>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Update the orbit in the 2D plane
         new_orbit_2D();
+
+        // Reorient the plane of the orbit in 3D
+        transform.position = ECI_mat.MultiplyVector(pos);
     }
 
     // Calculates the x and z possitions (orbit in 2D) through the eccentric_anomaly_angle E
@@ -56,7 +85,6 @@ public class Satellite_Orbit : MonoBehaviour
         eccentric_anomaly_angle(game_time.total_seconds);
         pos.x = a * (Mathf.Cos(E) - e);
         pos.z = b * Mathf.Sin(E);
-        transform.position = pos;
     }
 
     //This iterativelly calculates the True Anomaly E
