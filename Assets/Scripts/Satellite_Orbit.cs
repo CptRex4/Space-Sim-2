966cd94117514051dbfe2b9ddf89c322835ae783
Adam Bissett <abissett96@gmail.com>
2021-03-26 12:38:06 -0700

    Public Satellite
    Made the pos variable for the satellite public. Fixed a bug where the y-value was exploding. Clarified units in the comments.

--- Assets/Scripts/Satellite_Orbit.cs
+++ Assets/Scripts/Satellite_Orbit.cs
@@ -7,14 +7,14 @@ public class Satellite_Orbit : MonoBehaviour
     /*** Variables ***/
 
     // Physical Constants
-    static float earth_rad = 63.71f; // R_E * 10^(-5)
-    static float mu = 6.673f * 5.972f * Mathf.Pow(10, 24 - 11 - 5 * 3); // G * M_E * (10^(-5))^3
+    static float earth_rad = 63.71f; // R_E * 10^(-5) (unity distance)
+    static float mu = 6.673f * 5.972f * Mathf.Pow(10, 24 - 11 - 5 * 3); // G * M_E * (10^(-5))^3 (unity distance)
 
     // Orbital Parameters
-    float ra;   // Apogee radius
-    float rp;   // Perigee radius
-    float a;    // Semimajor axis
-    float b;    // Semiminor axis
+    float ra;   // Apogee radius in unity units
+    float rp;   // Perigee radius in unity units
+    float a;    // Semimajor axis in unity units
+    float b;    // Semiminor axis in unity units
     float e;    // Eccentricity
     float n;    // Mean motion
     float E;    // True anomilly 
@@ -26,15 +26,15 @@ public class Satellite_Orbit : MonoBehaviour
 
     // Calulation Variables
     int repititions = 100;  // Number of iterations to aproximate E
-    Vector3 pos;            // Position of the satelite
+    public Vector3 pos;     // Position of the satelite
 
     // Public Orbital Parameters
-    public float apogee;
-    public float perigee;
+    public float apogee;    // in meters
+    public float perigee;   // in meters
     //public float period;
-    public float inc;       // Inclination angle
-    public float omega;     // Right ascension of ascending node
-    public float w;         // Argument of perigree
+    public float inc;       // Inclination angle in degrees
+    public float omega;     // Right ascension of ascending node in degrees
+    public float w;         // Argument of perigree in degrees
 
     // Time Variables
     public GameObject clock;
@@ -74,24 +74,29 @@ public class Satellite_Orbit : MonoBehaviour
         update_orientation();
 
         // Reorient the plane of the orbit in 3D
-        transform.position = ECI_mat.MultiplyVector(pos);
+        pos = ECI_mat.MultiplyVector(pos);
+        transform.position = pos;
     }
 
     // Calculates the x and z possitions (orbit in 2D) through the eccentric_anomaly_angle E
     void new_orbit_2D() 
     {
+        // Compute E
         eccentric_anomaly_angle(game_time.total_seconds);
+
+        // Compute the 2D position of the satellite
         pos.x = a * (Mathf.Cos(E) - e);
         pos.z = b * Mathf.Sin(E);
+        pos.y = 0;
     }
 
     // Calculate a new orientation
     void update_orientation()
     {
         // Calculate quaternions for (inc, omega, w)
-        q_w = Quaternion.Euler(0f, w, 0f);          // rotate w radians about y-axis
-        q_inc = Quaternion.Euler(inc, 0f, 0f);      // rotate inc radians about x-axis
-        q_omega = Quaternion.Euler(0f, omega, 0f);  // rotate omega radians about y-axis
+        q_w = Quaternion.Euler(0f, w, 0f);          // rotate w degrees about y-axis
+        q_inc = Quaternion.Euler(inc, 0f, 0f);      // rotate inc degrees about x-axis
+        q_omega = Quaternion.Euler(0f, omega, 0f);  // rotate omega degrees about y-axis
 
         // Calculate the orientation matrix for the orbital plane
         ECI_mat = Matrix4x4.Rotate(q_omega) * Matrix4x4.Rotate(q_inc) * Matrix4x4.Rotate(q_w);
