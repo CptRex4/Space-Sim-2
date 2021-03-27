a296e70777fb293d3a1bf53a41bd8b599a83f4f9
Adam Bissett <abissett96@gmail.com>
2021-03-27 02:13:26 -0700

    Fixed units and changed orbital parameters
    Fixed the units for E and M to radians in all calculations and added comments. Changed the public orbital parameters to M_0, e, n, inc, omega, and w. All other orbital parameters are determined from these. rp and ra are public so the player can see them (future UI addition). Added the update_axes() function to handle axis parameter calculations. Added vel variable for future deltaV features.

--- Assets/Scripts/Satellite_Orbit.cs
+++ Assets/Scripts/Satellite_Orbit.cs
@@ -7,34 +7,38 @@ public class Satellite_Orbit : MonoBehaviour
     /*** Variables ***/
 
     // Physical Constants
-    static float earth_rad = 63.71f; // R_E * 10^(-5) (unity distance)
+    //static float earth_rad = 63.71f; // R_E * 10^(-5) (unity distance)
     static float mu = 6.673f * 5.972f * Mathf.Pow(10, 24 - 11 - 5 * 3); // G * M_E * (10^(-5))^3 (unity distance)
 
-    // Orbital Parameters
-    float ra;   // Apogee radius in unity units
-    float rp;   // Perigee radius in unity units
-    float a;    // Semimajor axis in unity units
-    float b;    // Semiminor axis in unity units
-    float e;    // Eccentricity
-    float n;    // Mean motion
-    float E;    // True anomilly 
-    float M;    // Mean anomilly
+    // Private Orbital Parameters
+    float a;    // Semimajor axis in unity distance units
+    float b;    // Semiminor axis in unity distance units
+    float E;    // True anomilly in radians
+    float M;    // Mean anomilly in radians
     Quaternion q_w,     // w quaternion
         q_inc,          // inc quaternion
         q_omega;        // omega quaternion
     Matrix4x4 ECI_mat;  // ECI orientation matrix
 
+    // Helpful Public Variables
+    public float ra;   // Apogee radius in meters
+    public float rp;   // Perigee radius in meters
+
     // Calulation Variables
     int repititions = 100;  // Number of iterations to aproximate E
-    public Vector3 pos;     // Position of the satelite
+    public Vector3 pos;     // Position of the satellite
+    public Vector3 vel;     // Velocity of the satellite
 
     // Public Orbital Parameters
-    public float apogee;    // in meters
-    public float perigee;   // in meters
+    //public float apogee;
+    //public float perigee;
     //public float period;
-    public float inc;       // Inclination angle in degrees
-    public float omega;     // Right ascension of ascending node in degrees
-    public float w;         // Argument of perigree in degrees
+    public float e;     // Eccentricity; < 0.25 for LEO
+    public float M_0;   // Initial mean anomilly in degrees
+    public float n;     // Mean motion in revolutions/day; > 11.25 for LEO
+    public float inc;   // Inclination in degrees
+    public float omega; // Right ascension of ascending node in degrees
+    public float w;     // Argument of perigree in degrees
 
     // Time Variables
     public GameObject clock;
@@ -50,13 +54,16 @@ public class Satellite_Orbit : MonoBehaviour
         // Calculate the initial orientation of the orbit
         update_orientation();
 
+        // Calculate the initial axis parameters
+        update_axes();
+
         // Defining the values of the orbital parameters
-        ra = earth_rad + apogee / 100000.0f;
-        rp = earth_rad + perigee / 100000.0f;
-        a = (ra + rp) / 2;
-        b = Mathf.Sqrt(ra * rp);
-        e = ra / a - 1;
-        n = Mathf.Sqrt(mu / Mathf.Pow(a, 3));
+        //ra = earth_rad + apogee / 100000.0f;
+        //rp = earth_rad + perigee / 100000.0f;
+        //a = (ra + rp) / 2;
+        //b = Mathf.Sqrt(ra * rp);
+        //e = ra / a - 1;
+        //n = Mathf.Sqrt(mu / Mathf.Pow(a, 3));
 
         // Getting game_time
         // game_time.total_seconds is the total game-world seconds since the start of the program
@@ -81,7 +88,7 @@ public class Satellite_Orbit : MonoBehaviour
     // Calculates the x and z possitions (orbit in 2D) through the eccentric_anomaly_angle E
     void new_orbit_2D() 
     {
-        // Compute E
+        // Compute the True Anomilly
         eccentric_anomaly_angle(game_time.total_seconds);
 
         // Compute the 2D position of the satellite
@@ -102,16 +109,32 @@ public class Satellite_Orbit : MonoBehaviour
         ECI_mat = Matrix4x4.Rotate(q_omega) * Matrix4x4.Rotate(q_inc) * Matrix4x4.Rotate(q_w);
     }
 
+    // Calculate the axis parameters of the elipse
+    void update_axes()
+    {
+        // Calculate values of (a, b, ra, rp) given (n, e)
+        a = Mathf.Pow(mu, 1f / 3f) / Mathf.Pow(2 * Mathf.PI * n / (24f * 3600f), 2f / 3f);    // Convert n to radians per second
+        ra = a * (1 + e);
+        rp = a * (1 - e);
+        b = Mathf.Sqrt(ra * rp);
+
+        // Convert (ra, rp) to meters
+        ra *= 100000f;
+        rp *= 100000f;
+    }
+
     //This iterativelly calculates the True Anomaly E
     void eccentric_anomaly_angle(float time) 
     {
-        float e_deg = e * Mathf.Rad2Deg;
-        M = n * time; //2 * Mathf.PI * time / (period * 60.0f);
+        // Calculate the current meant anomilly in radians
+        M = (2 * Mathf.PI * n / (24f * 3600f)) * time + Mathf.Deg2Rad * M_0;  // Convert n to radians per second and M_O to radians
+
+        // Initialize E to M and approximate E
         E = M;
 
         for (int i = 0; i < repititions; i++)
         {
-            E = E + (M + e_deg * Mathf.Sin(Mathf.Deg2Rad * E) - E) / (1 - e * Mathf.Cos(Mathf.Deg2Rad * E));
+            E = E + (M + e * Mathf.Sin(E) - E) / (1 - e * Mathf.Cos(E));
         }
     }
 }
