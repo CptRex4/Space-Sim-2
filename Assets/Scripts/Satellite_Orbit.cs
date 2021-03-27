1f1107cf98426ee4c3dd945f1e4f41c691826aad
Adam Bissett <abissett96@gmail.com>
2021-03-27 14:44:40 -0700

    Altitude and Speed
    Added public variables for altitude and speed for the player to see. "Helpful" variables and earth_rad are now in kilometers. Renamed update_axes() to update_axis_variables() and added the update_trajectory_variables() function. Everything can be updated in real time from unity (this needs to be changed in the game for efficiency).

--- Assets/Scripts/Satellite_Orbit.cs
+++ Assets/Scripts/Satellite_Orbit.cs
@@ -7,14 +7,18 @@ public class Satellite_Orbit : MonoBehaviour
     /*** Variables ***/
 
     // Physical Constants
-    static float earth_rad = 6378137f; // Radius of the earth in meters
+    static float earth_rad = 6378.137f; // Radius of the earth in kliometers
     static float mu = 0.3986004418f; // G * M_E * (10^(-5))^3 (unity distance conversion)
 
     // Private Orbital Parameters
     float a;    // Semimajor axis in unity distance units
     float b;    // Semiminor axis in unity distance units
+    float p;    // Semilatus rectum in unity distance units
     float ra;   // Apogee radius in unity distance units
     float rp;   // Perigee radius in unity distance units
+    // Dynamic Parameters
+    float r;    // Distance from origin in unity distance units
+    float v;    // Speed in unity distance units per second
     float E;    // True anomilly in radians
     float M;    // Mean anomilly in radians
     Quaternion q_w,     // w quaternion
@@ -23,9 +27,12 @@ public class Satellite_Orbit : MonoBehaviour
     Matrix4x4 ECI_mat;  // ECI orientation matrix
 
     // Helpful Public Variables
-    public float apogee;    // Apogee altitude in meters
-    public float perigee;   // Perigee altitude in meters
+    public float apogee;    // Apogee altitude in kilometers
+    public float perigee;   // Perigee altitude in kilometers
     public float period;    // Period in minutes
+    // Dynamic Parameters
+    public float altitude;  // Altitude in kilometers
+    public float speed;     // Speed in kilometers/second
 
     // Calulation Variables
     int repititions = 100;  // Number of iterations to aproximate E
@@ -41,10 +48,8 @@ public class Satellite_Orbit : MonoBehaviour
     public float w;     // Argument of perigree in degrees
 
     // Time Variables
-    public GameObject clock;
-
-    // Game_Time script
-    Game_Time game_time;
+    public GameObject clock;    // Clock object
+    Game_Time game_time;        // Game_Time script
 
     /*** Events ***/
 
@@ -55,7 +60,7 @@ public class Satellite_Orbit : MonoBehaviour
         update_orientation();
 
         // Calculate the initial axis parameters
-        update_axes();
+        update_axis_variables();
 
         // Getting game_time
         // game_time.total_seconds is the total game-world seconds since the start of the program
@@ -69,15 +74,17 @@ public class Satellite_Orbit : MonoBehaviour
         // Update the orbit in the 2D plane
         new_orbit_2D();
 
-        // Update the orientation of the orbit (probably inefficient; should be changed to only update when needed)
+        /*** Test Code: This is inefficient and should only be used in small-scale testing ***/
         update_orientation();
+        update_axis_variables();
+        update_trajectory_variables();
 
         // Reorient the plane of the orbit in 3D
         pos = ECI_mat.MultiplyVector(pos);
         transform.position = pos;
     }
 
-    // Calculates the x and z possitions (orbit in 2D) through the eccentric_anomaly_angle E
+    // Calculates the x and z coordinates (P and Q in perifocal coordinates) using E
     void new_orbit_2D() 
     {
         // Compute the True Anomilly
@@ -102,18 +109,31 @@ public class Satellite_Orbit : MonoBehaviour
     }
 
     // Calculate the axis parameters of the elipse
-    void update_axes()
+    void update_axis_variables()
     {
-        // Calculate values of (a, b, ra, rp) given (n, e)
+        // Calculate values of (a, b, ra, rp, p) given (n, e)
         a = Mathf.Pow(mu, 1f / 3f) / Mathf.Pow(2 * Mathf.PI * n / (24f * 3600f), 2f / 3f);    // Convert n to radians per second
+        p = a * (1 - e * e);
         ra = a * (1 + e);
         rp = a * (1 - e);
         b = Mathf.Sqrt(ra * rp);
 
         // Calculate helpful pulbic variables
-        apogee = ra * 100000f - earth_rad;      // Convert from unity distance to meters (10^5)
-        perigee = rp * 100000f - earth_rad;
-        period = (24f * 60f) / n;
+        apogee = ra * 100f - earth_rad;     // Convert from unity distance to kilometers (100)
+        perigee = rp * 100f - earth_rad;
+        period = (24f * 60f) / n;           // Convert from days to minutes
+    }
+
+    // Calutate the distance, altitude, and speed
+    void update_trajectory_variables()
+    {
+        // Calculate the speed and distance in unity units
+        r = p / (1 + e * Mathf.Cos(E));
+        v = Mathf.Sqrt(mu * ( (2 / r) - (1 / a) ));
+
+        // Calculate the altitude and speed in kilometers (convert from unity units)
+        altitude = r * 100 - earth_rad;
+        speed = v * 100;
     }
 
     //This iterativelly calculates the True Anomaly E
