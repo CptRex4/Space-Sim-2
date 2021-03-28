93c803751e995c3bfdeb1095fba93a0b803b10a7
WilliamBlack99 <iamwill55@hotmail.com>
2021-03-27 22:16:45 -0700

    weird commit

--- Assets/Scripts/Satellite_Selector.cs
+++ Assets/Scripts/Satellite_Selector.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class Satellite_Selector : MonoBehaviour
 {
+    public bool is_selected;
     GameObject satellite;
+    public string sat_name;
+    static public bool reset_selection = false;
 
     // Start is called before the first frame update
     void Start()
     {
         satellite = this.gameObject;
+        is_selected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (reset_selection)
+        {
+            print(1);
+        }
     }
+
     void OnMouseDown()
     {
+        is_selected = true;
         Camera_Control.change_target(satellite.GetComponent<Satellite_Orbit>());
+        //show stuff
     }
 }
